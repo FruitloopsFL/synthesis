a5399fdd6967ba9f87285d894e6cdeb90248be47
Kabir Sonne <kabir@sonne.co.za>
2020-03-15 17:35:12 +0200

    added more

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,73 +1,79 @@
 ﻿module Synthesis
 
 let abelar a =
-    let b = 
-        match (a % 12, a > 12, a < 3097) with 
-        | (0, true, true) -> true
-        | _ -> false
-    b
+    match (a % 12, a > 12, a < 3097) with 
+    | (0, true, true) -> true
+    | _ -> false
+
     
 let area b h =
-    let a =
-        match (b >= 0.0, h >= 0.0) with
-        | (true, true) -> 0.5 * b * h
-        | _ -> failwith "negative number"    
-    a
+    match (b >= 0.0, h >= 0.0) with
+    | (true, true) -> 0.5 * b * h
+    | _ -> failwith "negative number"    
+
     
 let zollo a =
-    let b = 
-        match (a >= 0) with
-        | true -> a * 2
-        | false -> -a
-    b
-
-let min a b =
-    let c = min a b
-    c
-
-let max a b =
-    let c = max a b
-    c
-
-let ofTime h m s =
-    s + (m * 60) + (h * 60 * 60)
+    match (a >= 0) with
+    | true -> a * 2
+    | false -> -a
     
 
-let toTime _ =
-    failwith "Not implemented"
+let min a b =
+    match a > b with
+    | true -> b
+    | false -> a
 
-let digits _ =
-    failwith "Not implemented"
+
+let max a b =
+    match a > b with
+    | true -> a
+    | false -> b
+
+
+let ofTime h m s =
+    s + (m * 60) + (h * 3600)
+    
+
+let toTime s =
+    match s < 0 with
+    | true -> (0, 0, 0)
+    | false -> (s/3600, (s%3600)/60, (s%3600)%60)
+    
+
+let digits x =
+    let rec counter x v =
+        match (x / 10 >= 1 || x / 10 <= -1) with
+        | false -> v
+        | true -> counter (x / 10) (v + 1)
+    counter x 1
 
 let minmax (a, b, c, d) =
     (min (min a b) (min c d), max (max a b) (max c d))
 
 let isLeap y =
-    let a =
-        match (y > 1581, y % 4 = 0, y % 400 = 0, y % 100 = 0) with
-        | (true, true, true, true) -> true
-        | (true, true, _, false) -> true
-        | (false, _, _, _)-> failwith "Unsupported input"
-        | _ -> false
-    a
+    match (y > 1581, y % 4 = 0, y % 400 = 0, y % 100 = 0) with
+    | (true, true, true, true) -> true
+    | (true, true, _, false) -> true
+    | (false, _, _, _)-> failwith "Unsupported input"
+    | _ -> false
+    
 
 let month a =
-    let b = 
-        match a with
-        |1 -> ("January", 31)
-        |2 -> ("February", 28)
-        |3 -> ("March", 31)
-        |4 -> ("April", 30)
-        |5 -> ("May", 31)
-        |6 -> ("June", 30)
-        |7 -> ("July", 31)
-        |8 -> ("August", 31)
-        |9 -> ("September", 30)
-        |10 -> ("October", 31)
-        |11 -> ("November", 30)
-        |12 -> ("December", 31)
-        |_ -> failwith "Invalid month"
-    b
+    match a with
+    |1 -> ("January", 31)
+    |2 -> ("February", 28)
+    |3 -> ("March", 31)
+    |4 -> ("April", 30)
+    |5 -> ("May", 31)
+    |6 -> ("June", 30)
+    |7 -> ("July", 31)
+    |8 -> ("August", 31)
+    |9 -> ("September", 30)
+    |10 -> ("October", 31)
+    |11 -> ("November", 30)
+    |12 -> ("December", 31)
+    |_ -> failwith "Invalid month"
+   
 
 let toBinary _ =
     failwith "Not implemented"
@@ -75,7 +81,7 @@ let toBinary _ =
 let bizFuzz _ =
     failwith "Not implemented"
 
-let monthDay _ _ =
+let monthDay d y =
     failwith "Not implemented"
 
 let coord _ =
